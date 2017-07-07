23848659ae0ad8d5b554f9a160093800939701d6
lzx李子翔 <zixiangli@ctrip.com>
2017-07-07 16:48:25 +0800

    用vue实现 打字效果

---
+++ resume/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+        <p>{{code}}</p>
+        <div v-html="computeStyle"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data () {
+            return {
+                code: ``,
+                finalCode: `#app {width:600px;margin:0 auto;font-family: 'Avenir', Helvetica, Arial, sans-serif;-webkit-font-smoothing: antialiased;-moz-osx-font-smoothing: grayscale;text-align: center;color: #2c3e50;margin-top: 60px;background: #ccc;}`
+            }
+        },
+        created(){
+            let n = 0;
+            let timer = setInterval(() => {
+                if (n === this.finalCode.length) {
+                    clearInterval(timer);
+                    console.log(`end`);
+                }
+                this.code = this.finalCode.substr(0, n);
+                n++;
+            }, 60)
+        },
+        computed: {
+            computeStyle(){
+                return `<style>${this.code}</style>`;
+            }
+        }
+    }
+</script>
+
